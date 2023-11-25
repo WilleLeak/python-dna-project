114cfde9d36c01d5aef6ef82431fc6c0528b8469
Evan Farnsworth <farnsworth.evan@yahoo.com>
2023-11-25 13:14:14 -0800

    initial commit

--- GUI.py
+++ GUI.py
@@ -1,1 +1,28 @@
+#GUI
+
+import PySimpleGUI as sg
+import dna_analysis_stuff as dna
+
+layout = [
+    [sg.Text('Enter DNA sequence')],
+    [sg.InputText(key = 'sequence_input')],
+    [sg.Button('Analyze')],
+    [sg.Output(size = (50, 10), key = 'analysis_output')]
+]
+
+window = sg.Window('DNA ANALYSIS GUI', layout, size = (500, 500))
+
+while True:
+  event, values = window.read()
+
+  if event == sg.WIN_CLOSED or event == 'Exit':
+    break
+
+  if event == 'Analyze':
+    dna_input = values['sequence_input']
+    dna_object_instance = dna.DNA(dna_input)
+    
+    print(f'the mrna strand is {dna_object_instance.mrnaStand}')
+
+window.close()
 
--- dna_analysis_stuff.py
+++ dna_analysis_stuff.py
@@ -1,1 +1,64 @@
+#DNA STUFF
+# code starts here
 
+class DNA:
+  aminoAcidDict = {'Phe': ['UUU', 'UUC'],
+                   'Leu': ['UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'],
+                   'Ile': ['AUU', 'AUC', 'AUA'],
+                   'Met': ['AUG'],
+                   'Val': ['GUU', 'GUC', 'GUA', 'GUG'],
+                   'Ser': ['UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'],
+                   'Pro': ['CCU', 'CCC', 'CCA', 'CCG'],
+                   'Thr': ['ACU', 'ACC', 'ACA', 'ACG'],
+                   'Ala': ['GCU', 'GCC', 'GCA', 'GCG'],
+                   'Tyr': ['UAU', 'UAC'],
+                   'Stop': ['UAA', 'UAG', 'UGA'],
+                   'His': ['CAU', 'CAC'],
+                   'Gln': ['CAA', 'CAG'],
+                   'Asn': ['AAU', 'AAC'],
+                   'Lys': ['AAA', 'AAG'],
+                   'Asp': ['GAU', 'GAC'],
+                   'Glu': ['GAA', 'GAG'],
+                   'Cys': ['UGU', 'UGC'],
+                   'Trp': ['UGG'],
+                   'Arg': ['CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],
+                   'Gly': ['GGU', 'GGC', 'GGA', 'GGG']
+                   }
+
+  def __init__(self, dnaStrand):
+    self.dnaStrand = dnaStrand.upper()
+    self.mrnaStrand = self.transcribeDnaToRna(dnaStrand)
+    self.aminoAcids = self.translateAminoAcids(self.mrnaStrand)
+    self.gcContentValue = self.gcContent(dnaStrand)
+
+  @staticmethod
+  def transcribeDnaToRna(dnaStrand):
+    dnaStrand = dnaStrand.lower()
+    dnaStrand = dnaStrand.replace('a', 'U')
+    dnaStrand = dnaStrand.replace('t', 'A')
+    dnaStrand = dnaStrand.replace('c', 'G')
+    dnaStrand = dnaStrand.replace('g', 'C')
+    return dnaStrand
+
+  @staticmethod
+  def translateAminoAcids(mrnaStrand):
+    aminoAcids = ''
+    for i in range(0, len(mrnaStrand) - 2, 3):
+      kmer = mrnaStrand[i:i+3]
+      for key, value in DNA.aminoAcidDict.items():
+        if kmer in value:
+          aminoAcids += key + ' '
+    return aminoAcids.strip()
+
+  @staticmethod
+  def gcContent(dnaStrand):
+    return round((dnaStrand.count('G') + dnaStrand.count('C')) / len(dnaStrand), 1)
+  
+
+
+# tests
+# d = DNA('TACGCATTAATT')
+
+# print(d.mrnaStrand)
+# print(d.aminoAcids)
+# print(d.gcContent)
